7954236a39e478c6769857ea9ea8fcd2decd04e3
Pardeep <118353650+PardeepSensation@users.noreply.github.com>
2023-08-17 09:34:06 +0530

    Mongoose Integration

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
-import express from "express";
-import mongoose from 'mongoose';
-import PostSchema from './models/post.js';
+import mongoose from "mongoose";
+import express from 'express';
 import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use(cors())
-
-app.get("/",async (req, res) => {
-  const dbPosts = await PostSchema.find();
-  res.json({
-    Posts: dbPosts,
-  });
-});
-
-app.post("/create", async (req, res) => {
-  const dbPosts = await PostSchema.create(req.body);
-  res.json({
-    Posts: dbPosts,
-  });
-});
-
-app.put("/update/:id", async (req, res) => {
-  await PostSchema.findByIdAndUpdate(req.params.id,req.body);
-  res.send("Post Updated Successfully");
-});
-
-app.delete("/delete", (req, res) => {
-  varPosts = [];
-  console.log("Endpoint / is owrking...");
-
-  res.send("DELETE METHOD: Hello World.");
-});
-
-
-mongoose.connect("mongodb://localhost:27017").then(()=>{
-  console.log("Database Connected...")
+const PostSchema = mongoose.Schema({
+  title: String,
+  description: String
 })
 
-app.listen(5000, () => {
-  console.log("Server is running on PORT: ", 5000);
-});
+const Post = mongoose.model('Posts', PostSchema);
+
+// Base URL : http://localhost:5000/getPost
+const CONNECTION_URL = "mongodb+srv://test1:test1@test.3hls2cs.mongodb.net/"
+
+app.get('/', async (req,res)=>{
+  const posts = await Post.find();
+  res.json({ Posts: posts });
+})
+
+app.get('/:id', async (req,res)=>{
+  const posts = await Post.find({
+    _id: req.params.id
+  });
+  res.json({ Posts: posts });
+})
+
+app.post('/create', async (req,res)=>{
+  const newPost = await Post.create(req.body);
+  res.json({ data: newPost })
+})
+app.patch('/updatePost/:id', async (req,res)=>{
+  const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body);
+  res.json({ data: updatedPost });
+})
+app.delete('/deletePost/:id', async (req,res)=>{
+  await Post.findByIdAndDelete(req.params.id);
+  res.send("Post Deleted Successfully.")
+})
+
+mongoose.connect(CONNECTION_URL).then(()=>{
+  console.log("DB Connected...")
+})
+
+app.listen(5000, ()=>{
+  console.log("Server is running...")
+})
